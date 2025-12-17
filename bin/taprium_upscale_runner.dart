7126c5394ffdbaabbb1ce9ee8cff272e44faea4b
fengjiongmax <fengjiongmax@gmail.com>
2025-12-17 16:54:00 +0000

    initial commit

---
+++ bin/taprium_upscale_runner.dart
@@ -0,0 +1,5 @@
+import 'package:taprium_upscale_runner/taprium_upscale_runner.dart' as taprium_upscale_runner;
+
+void main(List<String> arguments) {
+  print('Hello world: ${taprium_upscale_runner.calculate()}!');
+}
---
+++ test/taprium_upscale_runner_test.dart
@@ -0,0 +1,8 @@
+import 'package:taprium_upscale_runner/taprium_upscale_runner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
