2d5e2ae28965043ec6f53fa1c796fbbf3ec0e316
fengjiongmax <fengjiongmax@gmail.com>
2025-12-19 06:48:55 +0000

    add log filter so logs will print from the compiled binary

--- lib/log.dart
+++ lib/log.dart
@@ -1,5 +1,14 @@
 import 'package:logger/web.dart';
 
+class ReleaseLogFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    // Only show Info, Warning, Error, and Fatal in the binary
+    return event.level.index >= Level.info.index;
+  }
+}
+
 final logger = Logger(
+  filter: ReleaseLogFilter(),
   printer: PrettyPrinter(dateTimeFormat: DateTimeFormat.dateAndTime),
 );
